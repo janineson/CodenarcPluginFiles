6f875eea4a5cd25dfdf3f4a31d23d768ba6f57b9
janineson <janineson.it@gmail.com>
2018-01-30 15:59:46 +0900

    update

--- codenarc-idea/lib/Rules/SpecificSubscriptionRule.groovy
+++ codenarc-idea/lib/Rules/SpecificSubscriptionRule.groovy
@@ -17,6 +17,7 @@ package org.codenarc.rule.security
 
 import org.codehaus.groovy.ast.MethodNode
 import org.codehaus.groovy.ast.expr.BinaryExpression
+import org.codehaus.groovy.ast.expr.ClosureExpression
 import org.codehaus.groovy.ast.expr.ConstantExpression
 import org.codehaus.groovy.ast.expr.MethodCallExpression
 import org.codehaus.groovy.ast.expr.VariableExpression
@@ -36,16 +37,52 @@ class SpecificSubscriptionRule extends AbstractAstVisitorRule {
 }
 
 class SpecificSubscriptionAstVisitor extends AbstractAstVisitor {
-    def eventName
-
+    def eventName, inputType
+    Set<String> inputTypeList= new HashSet<String>(Arrays.asList("enum", "number","text", "email", "bool", "password",
+    "phone", "decimal", "time"))
+    Set<String> inputNames= new HashSet<String>()
+    Set<String> capabilityOtherAttr= new HashSet<String>(Arrays.asList("airQuality","audioTrackData", "volume", "battery", "numberOfButtons",
+        "carbonDioxide", "color", "hue", "saturation", "colorTemperature","fineDustLevel","dustLevel","energy","eta","data","fanSpeed","latitude",
+       "longitude","method","accuracy", "altitudeAccuracy", "heading", "speed", "lastUpdateTime", "numberOfButtons", "illuminance","image",
+        "infraredLevel", "activities","currentActivity", "supportedInputSources", "level","status", "trackData","trackDescription","odorLevel",
+        "supportedMachineStates", "remainingTime","operationTime","ovenSetpoint","pH","power","refrigerationSetpoint","humidity","lqi",
+        "rssi","soundPressureLevel", "phraseSpoken","goal","steps","temperature","coolingSetpoint","coolingSetpointRange",
+        "supportedThermostatFanModes","heatingSetpoint","heatingSetpointRange","supportedThermostatModes","thermostatSetpoint",
+        "thermostatSetpointRange","schedule", "threeAxis","tvChannel","ultravioletIndex","stream","voltage",""))
 
     @Override
     void visitMethodCallExpression(MethodCallExpression call){
-        if(AstUtil.isMethodNamed(call, 'subscribe', 3)) {
-            eventName = call.arguments.expressions[1]
 
-            if (eventName instanceof ConstantExpression && !eventName.text.contains('.')){
-                addViolation(call, 'Subscription must be specific to the Event you are interested in.')
+        if(AstUtil.isMethodNamed(call, 'input')) {
+            if (call.arguments.expressions[2] instanceof ConstantExpression)
+                inputType = call.arguments.expressions[2]?.value
+
+            if (inputTypeList.contains(inputType)){
+                if (call.arguments.expressions[1] instanceof ConstantExpression)
+                    inputNames.add(call.arguments.expressions[1].value)
+
+            }
+
+        }
+
+        if(AstUtil.isMethodNamed(call, 'subscribe', 3)) {
+            //get the event name they want to subscribe to
+            if (call.arguments.expressions[1] instanceof ConstantExpression)
+                eventName = call.arguments.expressions[1]?.value
+            else
+                eventName = call.arguments.expressions[1]?.name
+
+            if (eventName != null){
+                //if it is not declared as an input name in the preferences subscription OR
+                // the attribute is  ENUM  (not other attribute type - they dont need to be specific because it is already specific)
+                // and the event is not specific, it is a violation
+                if (!inputNames?.contains(eventName)){
+                    if (!capabilityOtherAttr.contains(eventName))
+                        if (!eventName?.contains('.'))
+                            addViolation(call, 'Subscription must be specific to the Event you are interested in.')
+
+                }
+
             }
 
         }
